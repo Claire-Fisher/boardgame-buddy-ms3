21d9e7d49053d5f11ff49dbbcc7d6910e4a12fdf
Claire Fisher <cafisher83@yahoo.co.uk>
2024-04-10 14:41:59 +0100

    add: toggle functionality to collection/wishlist buttons on profile page

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
     $(".alert").alert();
+    $("#user-collection-btn").click(function () {
+        $("#user-collection-card").removeClass("hidden");
+        $("#user-wishlist-card").addClass("hidden");
+    });
+    $("#user-wishlist-btn").click(function () {
+      $("#user-collection-card").addClass("hidden");
+      $("#user-wishlist-card").removeClass("hidden");
+    });
 });
