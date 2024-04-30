22745fc15c6783bedc86b75b152e0de2e1561815
Claire-Fisher <cafisher83@yahoo.co.uk>
2024-04-30 19:38:07 +0100

    chore: remove hidden toggle class and js, as not currently needed

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,5 @@
 $(document).ready(function () {
   $(".alert").alert();
-  $("#user-collection-btn").click(function () {
-    $("#user-collection-card").removeClass("hidden");
-    $("#user-wishlist-card").addClass("hidden");
-  });
   $(".user-filter").change(function () {
     $("#search-form").submit();
   });
